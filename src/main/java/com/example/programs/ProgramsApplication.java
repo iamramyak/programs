41a6faac82599fa5fd17ddd5fb2d458541432ff9
iamramyak <iamramyak7@gmail.com>
2024-01-09 15:13:57 +0530

    programs

--- src/main/java/com/example/programs/ProgramsApplication.java
+++ src/main/java/com/example/programs/ProgramsApplication.java
@@ -3,9 +3,6 @@ package com.example.programs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 public class ProgramsApplication {
 
@@ -13,17 +10,6 @@ public class ProgramsApplication {
 
 		SpringApplication.run(ProgramsApplication.class, args);
 	}
-
-
-//	public void getCustomers() {
-//		List<Customer> customers = new ArrayList<>();
-//		Customer customers1 = new Customer();
-//		customers1.setId(1L);
-//		customers1.setName("Robert");
-//		customers1.setEmail("robert@gmail");
-//
-//	}
-
 }
 
 
